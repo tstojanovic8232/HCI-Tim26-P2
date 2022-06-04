cfcf4a205259f9ef25b3f0d3eb9c0e9203dc254c
Teodora Stojanovic <t.stojanovic8232@gmail.com>
2022-06-04 17:51:27 +0200

    Update .gitignore

--- Projeakt2Interakcija/Projeakt2Interakcija/MainWindow.xaml.cs
+++ Projeakt2Interakcija/Projeakt2Interakcija/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Projeakt2Interakcija.Model;
+
 namespace Projeakt2Interakcija
 {
     /// <summary>
