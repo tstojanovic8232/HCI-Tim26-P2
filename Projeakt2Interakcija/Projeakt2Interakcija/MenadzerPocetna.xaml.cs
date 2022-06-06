1acc60327b68db6c9c469a99b89028dbaa7247d5
MilosBujakovic <40829867+MilosBujakovic@users.noreply.github.com>
2022-06-06 10:11:23 +0200

    Završena prva verzija interfejsa za Menadžera

--- Projeakt2Interakcija/Projeakt2Interakcija/MenadzerPocetna.xaml.cs
+++ Projeakt2Interakcija/Projeakt2Interakcija/MenadzerPocetna.xaml.cs
@@ -23,5 +23,45 @@ namespace Projeakt2Interakcija
         {
             InitializeComponent();
         }
+
+        void SetProperties()
+        {
+            this.Title = "Srbija Voz";
+
+            this.MinHeight = 600;
+            this.MinWidth = 800;
+            Uri iconUri = new Uri("./Slike/SrbijaVozLogo.jpg", UriKind.RelativeOrAbsolute);
+            this.Icon = BitmapFrame.Create(iconUri);
+        }
+
+        private void ProdateKarte_Click(object sender, RoutedEventArgs e)
+        {
+            PregledKarataMenadzer pregledKarata = new PregledKarataMenadzer();
+            pregledKarata.Show();
+        }
+
+        private void OdjaviMe_Click(object sender, RoutedEventArgs e)
+        {
+            OdjavaMenadzer logout = new OdjavaMenadzer();
+            logout.Show();
+        }
+
+        private void VozneLinije_Click(object sender, RoutedEventArgs e)
+        {
+            PregledVoznihLinijaMenadzer vozneLinije = new   PregledVoznihLinijaMenadzer();
+            vozneLinije.Show();
+        }
+
+        private void NasiVozovi_Click(object sender, RoutedEventArgs e)
+        {
+            NasiVozoviMenadzer nasiVozovi = new NasiVozoviMenadzer();
+            nasiVozovi.Show();
+        }
+
+        private void RedVoznje_Click(object sender, RoutedEventArgs e)
+        {
+            PregledRedaVoznje redVoznje = new PregledRedaVoznje();
+            redVoznje.Show();
+        }
     }
 }
---
+++ Projeakt2Interakcija/Projeakt2Interakcija/OdjavaMenadzer.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Projeakt2Interakcija
+{
+    /// <summary>
+    /// Interaction logic for OdjavaMenadzer.xaml
+    /// </summary>
+    public partial class OdjavaMenadzer : Window
+    {
+        public OdjavaMenadzer()
+        {
+            InitializeComponent();
+        }
+
+        private void PrijaviMe_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow pocetna = new MainWindow();
+            pocetna.Show();
+        }
+    }
+}
